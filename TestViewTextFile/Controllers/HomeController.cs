753d40ffb7e479c77db6cc191816c80977b10034
Tony_Tsai <tonytsai27@gmail.com>
2022-02-16 20:09:41 +0800

    加入專案檔案。

---
+++ TestViewTextFile/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestViewTextFile.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var texts = System.IO.File.ReadLines(Server.MapPath("~/App_Data/Sample.txt"));
+            ViewBag.Data = texts;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
